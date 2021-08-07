708d40ace26b93dfeef7cfd389a4e25ca66762df
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-08 00:10:48 +0900

    Add Slice

--- demo/demo_arrays.go
+++ demo/demo_arrays.go
@@ -8,6 +8,12 @@ func DemoArrays() {
 
 	demoArrayMultidimensional()
 
+	demoSlice()
+
+	demoMultipleSlice()
+
+	demoSliceLiteral()
+
 }
 
 
@@ -35,5 +41,48 @@ func demoArrayMultidimensional() {
 		}
 		fmt.Println()
 	}
+
+}
+
+
+func demoSlice() {
+
+	odd := [6]int{1, 3, 5, 7, 9, 11}
+	var s []int = odd[1:4]
+	fmt.Println(s)
+	odd[1] = 13
+	fmt.Println(s)
+	s[0] = 15
+	fmt.Println(s)
+	fmt.Println(odd)
+}
+
+func demoMultipleSlice() {
+
+	names := [4]string{"Apple", "Orange", "Banana",  "Kiwi"} 
+	 fmt.Println(names)
+	 slice1 := names[0:2] 
+	 slice2 := names[1:3] 
+	 fmt.Println(slice1, slice2) 
+	 slice2[0] = "Juice"
+	 fmt.Println(slice1, slice2) 
+	 fmt.Println(names)
+
+}
+
+func demoSliceLiteral() {
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{1, true},
+		{2, false},
+		{3, true},
+		{4, true},
+		{5, false},
+		{6, true},
+	}
+	fmt.Println(s)
 
 }
