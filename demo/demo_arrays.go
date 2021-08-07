2a13bd26ccb9dfbde1fb8cdbbd626e4aeba046cd
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-08 00:35:35 +0900

    Add Bounds

--- demo/demo_arrays.go
+++ demo/demo_arrays.go
@@ -14,6 +14,8 @@ func DemoArrays() {
 
 	demoSliceLiteral()
 
+	demoSliceBounds()
+
 }
 
 
@@ -86,3 +88,17 @@ func demoSliceLiteral() {
 	fmt.Println(s)
 
 }
+
+func demoSliceBounds() {
+
+	slice1 := []int{2,4,8,10,12,14}  
+	slice2 := slice1[2:4]  
+	fmt.Println(slice2)  
+	slice3 := slice1[:3]  
+	fmt.Println(slice3)  
+	slice4 := slice1[2:]  
+	fmt.Println(slice4)  
+	fmt.Println(slice1)  
+
+}
+
