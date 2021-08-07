60df45c241d5a8009424d0e17a773492e556fe48
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-07 16:05:41 +0900

    Add demo_functions

---
+++ demo/demo_functions.go
@@ -0,0 +1,14 @@
+package demo
+
+import fmt
+
+func DemoFunctions() {
+
+	fmt.Println("*************** Demo Functions ***************")
+
+
+}
+
+func demoFunction(x int, y int) {
+	fmt.Println("DemoFunction. %v %v", x, y)
+}
