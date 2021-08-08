9dc1146e32ba2acf5b27756266e4f43b42915440
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-09 00:16:17 +0900

    Add Demo String

--- demo/demo_strings.go
+++ demo/demo_strings.go
@@ -1,10 +1,29 @@
 package demo
 
+import "reflect"
+
 func DemoStrings() {
 
 	fmt.Println("*************** Demo Strings ***************")
 
+	demoStrings()
 
+	demoStringsLength()
 
 }
 
+
+func demoStrings() {
+
+	var x string = "sample string"  
+	fmt.Println(x)  
+	fmt.Println(reflect.TypeOf(x))  
+
+}
+
+func demoStringsLength() {
+
+	var x string = "sample string"  
+	fmt.Println(len(x))
+
+}
