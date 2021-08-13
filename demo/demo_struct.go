13696461c4658f07c598d7e01fdea76be59065ca
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-13 14:30:19 +0900

    Add demoStruct

--- demo/demo_struct.go
+++ demo/demo_struct.go
@@ -4,5 +4,19 @@ func DemoStruct() {
 
 	fmt.Println("*************** Demo Struct ***************")
 
-
+	demoStruct()
+	
 }
+
+	
+func demoStruct() {
+	c := Character{name: "Conan", race: "Human", }  
+	fmt.Println(c)  
+	fmt.Println(c.name) 
+}
+
+
+type Character struct {  
+	name string  
+	race  string  
+ }  
