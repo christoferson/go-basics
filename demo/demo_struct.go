908ca74cc98fb9b7fb5d27a7cc2e4c57f5b4f8dc
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-14 13:40:34 +0900

    Add Character2 struct

--- demo/demo_struct.go
+++ demo/demo_struct.go
@@ -5,9 +5,16 @@ func DemoStruct() {
 	fmt.Println("*************** Demo Struct ***************")
 
 	demoStruct()
+
+	demoStructEmbedded()
 	
 }
 
+
+type Character struct {  
+	name string  
+	race  string  
+}  
 	
 func demoStruct() {
 	c := Character{name: "Conan", race: "Human", }  
@@ -15,8 +22,18 @@ func demoStruct() {
 	fmt.Println(c.name) 
 }
 
-
-type Character struct {  
+type Character2 struct {  
 	name string  
 	race  string  
- }  
+}
+
+type NonPlayableCharacter2 struct {  
+	Character2
+	npcid  string  
+}
+
+func demoStructEmbedded() {
+	c := NonPlayableCharacter2{npcid: "578", Character2:Character2{name: "Conan", race: "Human"}}  
+	fmt.Println(c)  
+	fmt.Println(c.npcid) 
+}
