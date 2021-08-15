0428cb8ca39bdfc1e7cdf4eaf695c41339911a20
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-16 00:40:40 +0900

    Add demo_pointer

--- demo/demo_pointer.go
+++ demo/demo_pointer.go
@@ -4,6 +4,24 @@ func DemoPointer() {
 
 	fmt.Println("*************** Demo Pointer ***************")
 
-
+	demoPointer()
 	
 }
+
+//
+
+func demoPointer() {
+
+	x := 10
+
+	modifyIntPointer(&x)
+
+	fmt.Printf("Value of X is %v", x)
+
+}
+
+func modifyIntPointer(x *int) {  
+	*x = 5
+}
+
+//
