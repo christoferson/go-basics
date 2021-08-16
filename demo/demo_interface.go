19ddd1c59cdc5b37a36c59974262b201afbf543f
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-16 20:53:54 +0900

    Add interface

--- demo/demo_interface.go
+++ demo/demo_interface.go
@@ -5,7 +5,39 @@ func DemoInterface() {
 
 	fmt.Println("*************** Demo Interface ***************")
 
-
+	demoInterfaceMethod()
 	
 }
 
+//
+
+type ICharacter interface {  
+	Attack()  
+}
+
+type Character31 struct {  
+	name  string  
+	race  string  
+}
+
+func (c Character31) Attack()  {  
+	fmt.Printf("C31 %v attacks.", c.name)   
+}
+
+func icAttack(ic ICharacter) {
+	fmt.Println("C31 Calling ICharacter.Attack")
+	ic.Attack()  
+}
+
+func demoInterfaceMethod() {
+	c := Character31{name: "Conan", race: "Human"}  
+	c.Attack()
+	fmt.Println()
+	var i ICharacter = c
+	i.Attack()
+	fmt.Println()
+	icAttack(c)
+	fmt.Println()
+}
+
+//
