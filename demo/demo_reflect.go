a385d4f055ea294bb2b3e1d84197540a4d569d46
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-18 00:51:38 +0900

    Add Reflect

--- demo/demo_reflect.go
+++ demo/demo_reflect.go
@@ -1,9 +1,20 @@
 package demo
 
+import(  
+	"fmt"  
+	"reflect"  
+ )  
+
 func DemoReflect() {
 
 	fmt.Println("*************** Demo Reflect ***************")
 
-
+	demoReflect()
 	
+}
+
+func demoReflect() {
+	age := 27.5  
+	fmt.Printf("%T\n" ,age)  
+	fmt.Println(reflect.TypeOf(age))  
 }
