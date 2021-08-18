c1cae4360924609d9ddfde2d0be85a86d9719150
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-19 00:10:48 +0900

    Add demo map

--- demo/demo_map.go
+++ demo/demo_map.go
@@ -5,7 +5,17 @@ func DemoMap() {
 
 	fmt.Println("*************** Demo Map ***************")
 
+	demoMap()
+
+
 	
-	
+}
+
+func demoMap() {
+
+	characters := map[string]int{"Conan":28, "Natalya":37, "Griswold":20}  
+	fmt.Println(characters)  
+	fmt.Printf("characters['Natalya']=%v \n", characters["Natalya"])  
+
 }
 
