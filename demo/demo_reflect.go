5ab774af5bc60e66530833fbb0a2aa4fc781ad53
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-19 00:01:49 +0900

    Add Demo Rune

--- demo/demo_reflect.go
+++ demo/demo_reflect.go
@@ -10,6 +10,8 @@ func DemoReflect() {
 	fmt.Println("*************** Demo Reflect ***************")
 
 	demoReflect()
+
+	demoRune()
 	
 }
 
@@ -17,4 +19,10 @@ func demoReflect() {
 	age := 27.5  
 	fmt.Printf("%T\n" ,age)  
 	fmt.Println(reflect.TypeOf(age))  
+}
+
+func demoRune() {
+	rune := 'C'    
+	fmt.Printf("Type=%T Code=%d \n", rune, rune)    
+	fmt.Println(reflect.TypeOf(rune))
 }
