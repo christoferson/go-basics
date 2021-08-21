d4ff752af71765de0eaf57835bc95ff460ee0f42
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-22 01:09:27 +0900

    Add DemoErrorNew

--- demo/demo_error.go
+++ demo/demo_error.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"errors"
+)
+
 func DemoErrors() {
 
 	fmt.Println("*************** Demo Errors ***************")
 
+	DemoErrorNew()
+}
+
+func DemoErrorNew() {
+	var e = errors.New("Missing Value")
+	fmt.Println(e)
 }
