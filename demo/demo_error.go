a33d3fcc5e30c5f4020d80140ee8b3b9e30b6e84
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-22 16:05:21 +0900

    Add DemoErrorF

--- demo/demo_error.go
+++ demo/demo_error.go
@@ -9,9 +9,16 @@ func DemoErrors() {
 	fmt.Println("*************** Demo Errors ***************")
 
 	DemoErrorNew()
+	DemoErrorF()
 }
 
 func DemoErrorNew() {
 	var e = errors.New("Missing Value")
+	fmt.Println(e)
+}
+
+func DemoErrorF() {
+	var f = 829.2
+	var e = fmt.Errorf("math: square root of negative number %g", f)
 	fmt.Println(e)
 }
