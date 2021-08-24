330a5ee9ab25be1144257b5880d268d237f48780
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-25 00:52:10 +0900

    Add Recover Demo

--- demo/demo_recover.go
+++ demo/demo_recover.go
@@ -6,4 +6,24 @@ func DemoRecover() {
 
 	fmt.Println("*************** Demo Recover ***************")
 
+	demoRecover()
+
+}
+
+func demoRecover() {
+
+	fmt.Printf("Return Value: %v \n", demoRecoverDivide(1, 0))
+	fmt.Printf("Return Value: %v \n", demoRecoverDivide(30, 10))
+
+}
+
+func demoRecoverDivide(num1, num2 int) int {
+	defer func() {
+		e := recover()
+		if e != nil {
+			fmt.Printf("Recover from Exception. Msg=%v \n", e)
+		}
+	}()
+	quotient := num1 / num2
+	return quotient
 }
