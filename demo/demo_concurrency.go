4c9d9d4c1d234840d52131330ec3c8c567944ec9
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-08-29 21:29:24 +0900

    Add Mutex Example

--- demo/demo_concurrency.go
+++ demo/demo_concurrency.go
@@ -2,6 +2,7 @@ package demo
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -10,11 +11,15 @@ func DemoConcurrency() {
 
 	fmt.Println("*************** Demo Concurrency ***************")
 
-	demoConcurrency()
+	demoGoRoutine()
+	fmt.Println()
+	demoMutex()
 
 }
 
-func demoConcurrency() {
+// Demo Go Routine
+
+func demoGoRoutine() {
 
 	wg.Add(2)
 	go routine1()
@@ -39,4 +44,35 @@ func routine2() {
 		time.Sleep(time.Duration(5 * time.Millisecond))
 	}
 	wg.Done()
+}
+
+/// Demo using Mutex
+
+var waitMutex = sync.WaitGroup{}
+var count int
+var mutex sync.Mutex
+
+func demoMutex() {
+
+	waitMutex.Add(2)
+	go increment("foo: ")
+	go increment("bar: ")
+	waitMutex.Wait()
+	fmt.Println("Final count value ", count)
+
+}
+
+func increment(s string) {
+	for i := 0; i < 3; i++ {
+		mutex.Lock()
+		x := count
+		x++
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+		count = x
+		fmt.Println(s, i, "Count: ", count)
+		mutex.Unlock()
+
+	}
+	waitMutex.Done()
+
 }
