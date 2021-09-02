d49b4e96c8ecf53909ee6511da3e1070b86324c4
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-02 21:43:46 +0900

    Add Demo Time Components

--- demo/demo_time.go
+++ demo/demo_time.go
@@ -9,6 +9,8 @@ func DemoTime() {
 
 	fmt.Println("*************** Demo Time ***************")
 	demoCurrentTime()
+
+	demoTimeComponents()
 }
 
 func demoCurrentTime() {
@@ -17,5 +19,23 @@ func demoCurrentTime() {
 
 	present := time.Now() // current time
 	p(present)
+
+}
+
+func demoTimeComponents() {
+	fmt.Println("--- Try Time Components ---")
+	DOB := time.Date(2021, 07, 28, 15, 27, 42, 517, time.Local)
+	fmt.Println(DOB)
+
+	fmt.Println(DOB.Year())
+	fmt.Println(DOB.Month())
+	fmt.Println(DOB.Day())
+	fmt.Println(DOB.Hour())
+	fmt.Println(DOB.Minute())
+	fmt.Println(DOB.Second())
+	fmt.Println(DOB.Nanosecond())
+	fmt.Println(DOB.Location())
+
+	fmt.Println(DOB.Weekday())
 
 }
