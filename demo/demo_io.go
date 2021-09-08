d1a0701d27c8f9875339e4d53147fcdf2578dfb8
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-08 13:38:13 +0900

    Add Write File

--- demo/demo_io.go
+++ demo/demo_io.go
@@ -1,9 +1,28 @@
 package demo
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func DemoIO() {
 
 	fmt.Println("*************** Demo I/O ***************")
+
+	demoWriteFile()
+
+}
+
+func demoWriteFile() {
+
+	fmt.Println("--- Try Write File ---")
+
+	file, err := os.Create("tmp/file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.WriteString("Hi... there")
+	file.Close()
 
 }
