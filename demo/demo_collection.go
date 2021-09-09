c596cd6f2d51df30c8d781e15c5b53eead58fc86
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-09 23:04:18 +0900

    Add Sort Array

--- demo/demo_collection.go
+++ demo/demo_collection.go
@@ -1,8 +1,22 @@
 package demo
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func DemoCollection() {
 
 	fmt.Println("*************** Demo Collection ***************")
+
+	demoSortArray()
+}
+
+func demoSortArray() {
+
+	fmt.Println("--- Try Sort Array ---")
+
+	intValue := []int{57, 12, 20, 5, 8, 2}
+	sort.Ints(intValue)
+	fmt.Println("Ints:   ", intValue)
 }
