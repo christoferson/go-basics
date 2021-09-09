69e8a599f24d1139410537a1148cc301b1b2ea91
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-10 01:01:00 +0900

    Add Float Sort

--- demo/demo_collection.go
+++ demo/demo_collection.go
@@ -10,6 +10,8 @@ func DemoCollection() {
 	fmt.Println("*************** Demo Collection ***************")
 
 	demoSortIntArray()
+
+	demoSortFloatArray()
 }
 
 func demoSortIntArray() {
@@ -19,4 +21,15 @@ func demoSortIntArray() {
 	intValue := []int{57, 12, 20, 5, 8, 2}
 	sort.Ints(intValue)
 	fmt.Println("Ints:   ", intValue)
+
+}
+
+func demoSortFloatArray() {
+
+	fmt.Println("--- Try Float Sort Array ---")
+
+	floatValue := []float64{12.7, 25.1, 15.2, 18.32}
+	sort.Float64s(floatValue)
+	fmt.Println("floatValue:   ", floatValue)
+
 }
