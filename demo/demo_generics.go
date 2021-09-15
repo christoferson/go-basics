2feee24155974c7e0aa9015038a5c59000463fe1
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-16 01:08:47 +0900

    Save

--- demo/demo_generics.go
+++ demo/demo_generics.go
@@ -6,4 +6,12 @@ func DemoGenerics() {
 
 	fmt.Println("*************** Demo Generics ***************")
 
+	//demoGenericsPrint()
+
 }
+
+// func demoGenericsPrint[T any](s []T) {
+// 	for _, v := range s {
+// 		fmt.Print(v)
+// 	}
+// }
